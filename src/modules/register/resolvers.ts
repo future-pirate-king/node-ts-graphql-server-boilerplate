783d05fa56f7aae97491680841660e98b87a9cd7
Sam David <samd948@gmail.com>
2018-08-08 12:19:02 +0530

    modulerised resolvers & schemas

--- src/modules/register/resolvers.ts
+++ src/modules/register/resolvers.ts
@@ -1,13 +1,16 @@
 import * as bcrypt from 'bcryptjs';
-import { ResolverMap } from './types/graphql-utils';
-import { User } from './entity/User';
+import { User } from '../../entity/User';
+import { ResolverMap } from '../../types/graphql-utils';
 
 export const resolvers: ResolverMap = {
   Query: {
-    hello: (_, { name }: GQL.IHelloOnQueryArguments) => `Hello ${name || 'Sam David'}`,
+    bye: () => 'bye',
   },
   Mutation: {
-    register: async (_, { email, password }: GQL.IRegisterOnMutationArguments) => {
+    register: async (
+      _,
+      { email, password }: GQL.IRegisterOnMutationArguments
+    ) => {
       const hashedPassword = await bcrypt.hash(password, 10);
       const user = User.create({
         email,

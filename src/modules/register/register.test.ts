c46a49a2affadcd6d7d770b7370a32c4a071414b
Sam David <samd948@gmail.com>
2018-08-09 11:30:24 +0530

    handled other errors of email & password

--- src/modules/register/register.test.ts
+++ src/modules/register/register.test.ts
@@ -1,11 +1,17 @@
 import { request } from 'graphql-request';
 import { startServer } from '../../startServer';
 import { User } from '../../entity/User';
+import {
+  duplicateEmail,
+  emailNotLongEnough,
+  emailNotValid,
+  passwordNotLongEnough,
+} from './errorMessages';
 
 let getHost = () => '';
 
 beforeAll(async () => {
-  const app = await startServer();
+  const app: any = await startServer();
   const { port } = app.address();
   getHost = () => `http://127.0.0.1:${port}`;
 });
@@ -13,27 +19,82 @@ beforeAll(async () => {
 const email = 'samd851@mail.com';
 const password = 'gsfgsgsf';
 
-const mutation = `
+const mutation = (e: string, p: string) => `
   mutation {
-    register(email: "${email}", password: "${password}") {
+    register(email: "${e}", password: "${p}") {
       path
       message
     }
   }
 `;
 
-test('Register User', async () => {
-  const response = await request(getHost(), mutation);
-  expect(response).toEqual({ register: null });
+describe('Register User', async () => {
+  it('check for duplicate email', async () => {
+    // register new user
+    const response = await request(getHost(), mutation(email, password));
+    expect(response).toEqual({ register: null });
 
-  const users = await User.find({ where: { email } });
-  expect(users.length).toEqual(1);
+    const users = await User.find({ where: { email } });
+    expect(users.length).toEqual(1);
 
-  const user = users[0];
-  expect(user.email).toEqual(email);
-  expect(user.password).not.toEqual(password);
+    const user = users[0];
+    expect(user.email).toEqual(email);
+    expect(user.password).not.toEqual(password);
 
-  const response2: any = await request(getHost(), mutation);
-  expect(response2.register).toHaveLength(1);
-  expect(response2.register[0].path).toEqual('email');
+    // duplicate email
+    const response2: any = await request(getHost(), mutation(email, password));
+    expect(response2.register).toHaveLength(1);
+    expect(response2.register[0]).toEqual({
+      path: 'email',
+      message: duplicateEmail,
+    });
+  });
+
+  it('check for bad email', async () => {
+    const response3: any = await request(getHost(), mutation('sd', password));
+    expect(response3).toEqual({
+      register: [
+        {
+          path: 'email',
+          message: emailNotLongEnough,
+        },
+        {
+          path: 'email',
+          message: emailNotValid,
+        },
+      ],
+    });
+  });
+
+  it('check for bad password', async () => {
+    const response4: any = await request(getHost(), mutation(email, 'sd'));
+    expect(response4).toEqual({
+      register: [
+        {
+          path: 'password',
+          message: passwordNotLongEnough,
+        },
+      ],
+    });
+  });
+
+  it('check for bad password and email', async () => {
+    const response5: any = await request(getHost(), mutation('em', 'sd'));
+    expect(response5).toEqual({
+      register: [
+        {
+          path: 'email',
+          message: emailNotLongEnough,
+        },
+        {
+          path: 'email',
+          message: emailNotValid,
+        },
+        {
+          path: 'password',
+          message: passwordNotLongEnough,
+        },
+      ],
+    });
+  });
 });
